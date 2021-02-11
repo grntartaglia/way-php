7b9b22bac673f4d1defaa1939d5834ac02f2ceb2
Gabriel Tartaglia <ga.tartaglia96@gmail.com>
2021-02-11 11:32:01 -0300

    Adiciona configuração padrão

--- src/PhpCsFixer/Config.php
+++ src/PhpCsFixer/Config.php
@@ -3,9 +3,16 @@
 namespace Braip\WayPhp\PhpCsFixer;
 
 use PhpCsFixer;
+use PhpCsFixer\Finder;
 
 class Config extends PhpCsFixer\Config
 {
+    public function __construct(Finder $finder = null)
+    {
+        parent::__construct();
 
-    
+        $finder = $finder ?? Finder::create()->in(__DIR__);
+
+        $this->setFinder($finder);
+    }
 }
